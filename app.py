9e25b9795f0977270096e13f8a7a0458ed4b66b8
Avishek Bhattacharjee <avishek350751@gmail.com>
2024-08-18 08:16:54 -0500

    added multiply function

--- app.py
+++ app.py
@@ -6,8 +6,14 @@ def sub(a, b):
     return a - b
 
 
+def mul(a, b):
+    return a * b
+
+
 result1 = add(4, 6)
 result2 = sub(6, 4)
+result3 = mul(6, 4)
 
 
-print(result1, result2)
+print(result1, result2, result3)
+
