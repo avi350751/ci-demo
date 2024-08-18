e7daa58cb90c36d8f4455d5acbf1a9e723748b27
Avishek Bhattacharjee <avishek350751@gmail.com>
2024-08-17 23:52:20 -0500

    fixed git linting issue

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
-def add(a,b):
-    return a+b
+def add(a, b):
+    return a + b
 
 
+def sub(a, b):
+    return a - b
 
+result1 = add(4, 6)
+result2 = sub(6, 4)
 
-def sub(a,b):return a-b
-
-result1 = add(4,6)
-result2 = sub(6,4)
-
-print(result1)
-print(result2)
+print(result1,result2)
